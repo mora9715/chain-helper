1488900cde717b999b46d243e0a349314dd395b7
Yevhen Prodan <yevhen.prodan@namecheap.com>
2026-01-09 21:06:53 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Chain helper
-
-The project provides a tool to easier calculate different ring dimensions for chain weaving
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { ChainWeavingCalculator } from './components/ChainWeavingCalculator'
-
-function App() {
-  return <ChainWeavingCalculator />
-}
-
-export default App
